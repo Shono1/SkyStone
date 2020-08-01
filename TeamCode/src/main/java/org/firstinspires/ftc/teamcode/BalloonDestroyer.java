9bc5e188d38225950c9ce8da0cbdd2f6525ee2d7
Sam <shonor47@gmail.com>
2020-08-01 18:08:05 -0400

    Checkpoint 2: set up motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BalloonDestroyer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BalloonDestroyer.java
@@ -1,10 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class BalloonDestroyer extends LinearOpMode {
-    
-    public void runOpMode() {
+    private DcMotor frontLeft, backLeft, frontRight, backRight;
 
+    public void runOpMode() {
+        frontLeft = hardwareMap.get(DcMotor.class, "fl");
+        backLeft = hardwareMap.get(DcMotor.class, "bl");
+        frontRight = hardwareMap.get(DcMotor.class, "fr");
+        backRight = hardwareMap.get(DcMotor.class, "br");
+
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+        while(opModeIsActive()) {
+
+        }
     }
 }
